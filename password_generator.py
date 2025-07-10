80585e5326409831f377b59c2438d9f33feef4b2
rickey0123 <nabhoneeldhar@gmail.com>
2025-07-10 23:45:39 +0530

    first commit

---
+++ password_generator.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, jsonify, render_template
+import random
+import string
+
+app = Flask(__name__)
+
+def genpasswd(length=10):
+    if length < 4:
+        raise ValueError('password length too short')
+    lowercase = string.ascii_lowercase
+    uppercase = string.ascii_uppercase
+    numbers = string.digits
+    special = string.punctuation
+    password = [
+        random.choice(lowercase), random.choice(uppercase),
+        random.choice(numbers), random.choice(special)
+    ]
+
+    characters = lowercase + uppercase + numbers + special
+    password += random.choices(characters, k=length-4)
+    random.shuffle(password)
+    return ''.join(password)
+
+@app.route('/')
+def index():
+    return render_template('webpage.html')
+
+@app.route('/generate')
+
+def generate():
+    length = request.args.get('length', default=10)
+    try:
+        length = int(length)
+    except ValueError:
+        return jsonify(error='Invalid length'), 400
+    password = genpasswd(length)
+    return jsonify(password=password)
+
+if __name__ =='__main__':
+    app.run(debug=True)
+
